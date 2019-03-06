b16da3896a9070256fe7a3d5b9cf270d2fb9078e
edgrmrc <edgrmrc@gmail.com>
2019-03-06 20:14:22 +0000

    Initial Bookmark Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'welcome/Index'
+  get 'bookmarks/p'  
+  
+
+  resources :bookmarks
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  root 'welcome#index'
+end
